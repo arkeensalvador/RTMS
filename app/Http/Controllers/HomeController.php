6f767be8fb078f47923141d6c6b8179f2d0daf65
arkeensalvador <mitcharkeensalvador@gmail.com>
2023-08-15 13:20:25 +0800

    8/15/20
    Add chart js to dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,30 @@ class HomeController extends Controller
         ->join('users', 'users.agencyID', '=', 'agency.id')
         ->select('users.*', 'agency.*')
         ->get();
-        return view('backend.layouts.dashboard', compact('all','title'));
+
+        $title = 'List of Reports | RTMS';
+        $new = DB::table('programs')->where('program_status', '=', 'new')->pluck('program_status')->count();
+        $ongoing = DB::table('programs')->where('program_status', '=', 'on-going')->pluck('program_status')->count();
+        $terminated = DB::table('programs')->where('program_status', '=', 'terminated')->pluck('program_status')->count();
+        $completed = DB::table('programs')->where('program_status', '=', 'completed')->pluck('program_status')->count();
+        // Project
+        $new_proj = DB::table('projects')->where('project_status', '=', 'new')->pluck('project_status')->count();
+        $ongoing_proj = DB::table('projects')->where('project_status', '=', 'on-going')->pluck('project_status')->count();
+        $terminated_proj = DB::table('projects')->where('project_status', '=', 'terminated')->pluck('project_status')->count();
+        $completed_proj = DB::table('projects')->where('project_status', '=', 'completed')->pluck('project_status')->count();
+
+        // $agency = DB::table('agency')->get()->pluck("abbrev");
+        $data = DB::table('agency')->get();
+
+        foreach($data as $agency) {
+            $list[] = $agency->abbrev;
+        }
+
+        $total_new = $new + $new_proj;
+        $total_ongoing = $ongoing + $ongoing_proj;
+        $total_terminated = $terminated + $terminated_proj;
+        $total_completed = $completed + $completed_proj;
+        return view('backend.layouts.dashboard', ['total_new' => $total_new, 'total_ongoing' => $total_ongoing, 'total_terminated' => $total_terminated, 'total_completed' => $total_completed, 'list' => $list], compact('all','title'));
     }
 
     
--- resources/views/backend/layouts/dashboard.blade.php
+++ resources/views/backend/layouts/dashboard.blade.php
@@ -1,166 +1,277 @@
 @extends('backend.layouts.app')
 @section('content')
+    <div class="content-wrapper">
 
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ auth()->user()->role }} - Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="home">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-<div class="content-wrapper">
-    
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">{{ auth()->user()->role }} - Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="home">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Info boxes -->
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Info boxes -->
-            <div class="row">
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">CPU Traffic</span>
+                                <span class="info-box-number">
+                                    10
+                                    <small>%</small>
 
-                        <div class="info-box-content">
-                            <span class="info-box-text">CPU Traffic</span>
-                            <span class="info-box-number">
-                                10
-                                <small>%</small>
-                                
-                            </span>
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
+                        <!-- /.info-box -->
                     </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                    <!-- /.col -->
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
-                        <div class="info-box-content">
-                            <span class="info-box-text">Likes</span>
-                            <span class="info-box-number">41,410</span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Likes</span>
+                                <span class="info-box-number">41,410</span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
+                        <!-- /.info-box -->
                     </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
+                    <!-- /.col -->
 
-                <!-- fix for small devices only -->
-                <div class="clearfix hidden-md-up"></div>
+                    <!-- fix for small devices only -->
+                    <div class="clearfix hidden-md-up"></div>
 
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
-                        <div class="info-box-content">
-                            <span class="info-box-text">Sales</span>
-                            <span class="info-box-number">760</span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Sales</span>
+                                <span class="info-box-number">760</span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
+                        <!-- /.info-box -->
                     </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                    <!-- /.col -->
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
-                        <div class="info-box-content">
-                            <span class="info-box-text">New Members</span>
-                            <span class="info-box-number">2,000</span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">New Members</span>
+                                <span class="info-box-number">2,000</span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
+                        <!-- /.info-box -->
                     </div>
-                    <!-- /.info-box -->
+                    <!-- /.col -->
                 </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
+                <!-- /.row -->
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="card-title">{{ auth()->user()->role}} -  Dashboard</h5>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            {{-- <div class="card-header">
+                                <h5 class="card-title">{{ auth()->user()->role }} - Dashboard</h5>
 
-                            <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                    <i class="fas fa-minus"></i>
-                                </button>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                                        <i class="fas fa-wrench"></i>
+                                <div class="card-tools">
+                                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                        <i class="fas fa-minus"></i>
                                     </button>
-                                    
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
+                                            <i class="fas fa-wrench"></i>
+                                        </button>
+
+                                    </div>
+                                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                        <i class="fas fa-times"></i>
+                                    </button>
                                 </div>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                    <i class="fas fa-times"></i>
-                                </button>
+                            </div> --}}
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <div class="row">
+                                    {{-- <div class="col-md-8">
+                                        Welcome to {{ auth()->user()->role }} - Dashboard
+                                    </div> --}}
+                                    <!-- /.col -->
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="card card-success" >
+                                            <div class="card-header" style="background-color: #74c023;">
+                                                <h3 class="card-title">AIHRs</h3>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="card-body">
+                                                    <div class="chart">
+                                                        <canvas id="myChart"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- /.card-body -->
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="card card-success" >
+                                            <div class="card-header" style="background-color: #74c023;">
+                                                <h3 class="card-title">AIHRs</h3>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="card-body">
+                                                    <div class="chart">
+                                                        <canvas id="myChart"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- /.card-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="card card-success">
+                                            <div class="card-header" style="background-color: #74c023;">
+                                                <h3 class="card-title">AIHRs</h3>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="card-body">
+                                                    <div class="chart">
+                                                        <canvas id="myChart"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- /.card-body -->
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="card card-success">
+                                            <div class="card-header" style="background-color: #74c023;">
+                                                <h3 class="card-title">AIHRs</h3>
+                                            </div>
+                                            <div class="col-md-12">
+                                                <div class="card-body">
+                                                    <div class="chart">
+                                                        <canvas id="myChart"></canvas>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- /.card-body -->
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <!-- /.row -->
                             </div>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-8">
-                                    Welcome to {{ auth()->user()->role}} - Dashboard
-                                </div>
-                                <!-- /.col -->
-                            </div>
-                            <!-- /.row -->
-                        </div>
-                        <!-- ./card-body -->
-                        <div class="card-footer">
-                            <div class="row">
-                                <div class="col-sm-3 col-6">
-                                    
-                                    
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-sm-3 col-6">
-                                    
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-sm-3 col-6">
-                                    
-                                    <!-- /.description-block -->
-                                </div>
-                                <!-- /.col -->
-                                <div class="col-sm-3 col-6">
-                                    {{-- <div class="description-block">
+                            <!-- ./card-body -->
+                            <div class="card-footer">
+                                <div class="row">
+                                    <div class="col-sm-3 col-6">
+
+
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-sm-3 col-6">
+
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-sm-3 col-6">
+
+                                        <!-- /.description-block -->
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-sm-3 col-6">
+                                        {{-- <div class="description-block">
                                         <span class="description-percentage text-danger"><i
                                                 class="fas fa-caret-down"></i> 18%</span>
                                         <h5 class="description-header">1200</h5>
                                         <span class="description-text">GOAL COMPLETIONS</span>
                                     </div> --}}
-                                    <!-- /.description-block -->
+                                        <!-- /.description-block -->
+                                    </div>
                                 </div>
+                                <!-- /.row -->
                             </div>
-                            <!-- /.row -->
+                            <!-- /.card-footer -->
                         </div>
-                        <!-- /.card-footer -->
+                        <!-- /.card -->
                     </div>
-                    <!-- /.card -->
+                    <!-- /.col -->
                 </div>
-                <!-- /.col -->
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
-        </div>
-        <!--/. container-fluid -->
-    </section>
-    <!-- /.content -->
-</div>
+            <!--/. container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const ctx = document.getElementById('myChart');
+        const ctxx = document.getElementById('myChart2');
+
+        // var agencyList = {{ json_encode($list) }};
+        var totalNew = {{ json_encode($total_new) }};
+        var totalOngoing = {{ json_encode($total_ongoing) }};
+        var totalTerminated = {{ json_encode($total_terminated) }};
+        var totalCompleted = {{ json_encode($total_completed) }};
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['New', 'Ongoing', 'Terminated', 'Completed'],
+                datasets: [{
+                    label: "Total",
+                    data: [totalNew, totalOngoing, totalTerminated, totalCompleted],
+                    backgroundColor: [
+                        'rgba(255, 193, 7, 0.9)',
+                        'rgba(23, 162, 184, 0.9)',
+                        'rgba(179 , 0, 0, 0.9)',
+                        'rgba(0, 128, 0, 0.9)'
+                    ],
+                    borderWidth: 1
+                }],
+
+
+            },
+
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+
+                }
+            }
+        });
+    </script>
 @endsection
