3e42b3077a7410de72aed53211cf0723866f08f8
arkeensalvador <mitcharkeensalvador@gmail.com>
2023-09-22 16:04:40 +0800

    Update export_report.blade.php

--- resources/views/backend/reportlist/export_report.blade.php
+++ resources/views/backend/reportlist/export_report.blade.php
@@ -99,6 +99,27 @@
         @endforeach
     </table>
 
+    {{-- <h3>List of Sub-Projects</h3>
+    <table id="customers">
+        <tr>
+            <th>Sub-Project Title</th>
+            <th>Description</th>
+            <th>Duration(m/d/Y)</th>
+            <th>Funding Agency</th>
+        </tr>
+        @foreach ($splist as $spl)
+            <tr>
+                <td>{{ $spl->sub_project_title }}</td>
+                <td>{{ $spl->sub_project_description }}</td>
+                <td>
+                    {{ date('m/d/Y', strtotime($spl->sub_project_start_date)) ?: 'Not Set' }} -
+                    {{ date('m/d/Y', strtotime($spl->sub_project_end_date)) ?: 'Not Set' }}
+                </td>
+                <td>{{ $spl->sub_project_agency }}</td>
+            </tr>
+        @endforeach
+    </table> --}}
+
     
 
 </body>
