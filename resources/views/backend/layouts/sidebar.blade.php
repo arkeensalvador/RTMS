583a65c17ef7f22325cc65ae0ed1e517fa8712f4
arkeensalvador <mitcharkeensalvador@gmail.com>
2023-11-08 09:49:50 +0800

    Update sidebar.blade.php

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="home" class="brand-link">
-        <img src="{{ asset('img/CLAARRDEC.png') }}" alt="CLAARRDEC Logo" class="brand-image img-circle" style="opacity: .8">
+        <img src="{{ asset('img/claarrdec.png') }}" alt="CLAARRDEC Logo" class="brand-image img-circle" style="opacity: .8">
         <span class="brand-text font-weight-light">RTMS</span>
     </a>
 
@@ -11,7 +11,7 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 @if (auth()->user()->role == 'Admin')
-                    <img src="{{ asset('img/AdminLTElogo.png') }}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{ asset('img/adminltelogo.png') }}" class="img-circle elevation-2" alt="User Image">
                 @else
                     <img src="{{ asset('img/avatar4.png') }}" class="img-circle elevation-2" alt="User Image">
                 @endif
